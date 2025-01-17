3f0f5a8b2a6b09ee671bac90f07d2df63a4d083a
DeEmilyRose <emilyrangel0711@gmail.com>
2025-01-18 00:30:26 +0100

    TEST REALIZADOS

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -3,7 +3,10 @@ class ScoreCard:
 
     def __init__(self, score_card):
         self.pins = score_card
-
+        self.score = 0 #puntaje
+        self.frame = 10 #turnos
+        self.rolls = list(self.pins) #bolos tumbados ¿podemos convertir la lanzada en una lista para manejar cada lanzamiento por separado?
+        self.intentos = 0 #cambiar nombre
 
     def get_pins(self):
         return self.pins
--- test/test_score_card.py
+++ test/test_score_card.py
@@ -1,16 +1,36 @@
 from src.scoreCard import ScoreCard
 
+#OBJETO CREADO CORRECTAMENTE
 def test_score_card():
     card = ScoreCard('12345123451234512345')
     assert card 
 
+#TIRADAS ALMACENADAS CORRECTAMENTE
 def test_frame_pins():
     PINS = '12345123451234512345'
     card = ScoreCard(PINS)
     assert card.get_pins() == PINS
-    
-def total_score():
+
+#JUGADA SIMPLE (PUNTUACIÓN 60)
+def test_total_score():
     PINS = '12345123451234512345'
     card = ScoreCard(PINS)
     assert 60 == card.total_score()  
-    
+
+#PERFECT GAME (12 STRIKES)       
+def test_strike():
+    PINS = 'XXXXXXXXXXXX'
+    card = ScoreCard(PINS)
+    assert 300 == card.strike()      
+
+#FRAMES SPARES (10 PINES EN LOS DOS INTENTOS DEL FRAME)
+def test_spares():
+    PINS = '5/5/5/5/5/5/5/5/5/5/5'
+    card = ScoreCard(PINS)
+    assert 150 == card.spares()      
+
+#DESAMOR 9 PINES POR FRAME
+def test_heartbreak():
+    PINS = '9-9-9-9-9-9-9-9-9-9-'  
+    card = ScoreCard(PINS)
+    assert card.heartbreak() == 90    
