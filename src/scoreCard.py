1d9684ad040af43f6371b038f8ca20b433b22d6e
DeEmilyRose <emilyrangel0711@gmail.com>
2025-01-18 21:34:47 +0100

    función de jugada simple

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -2,17 +2,16 @@
 class ScoreCard:
 
     def __init__(self, score_card):
-        self.pins = score_card
+        self.pins = score_card #lanzamientos
+
         self.score = 0 #puntaje
         self.frame = 10 #turnos
-        self.rolls = list(self.pins) #bolos tumbados ¿podemos convertir la lanzada en una lista para manejar cada lanzamiento por separado?
         self.intentos = 0 #cambiar nombre
 
     def get_pins(self):
         return self.pins
     
-    def total_score(self, pins):
-        for pin in pins:
-            int(pin)
-            total = sum(pin)
-            return total
+    def total_score(self):
+      return sum(int(pin) for pin in self.pins)
+         #separamos los lanzamientos y los convertimos
+         #en números enteros para sumamos
