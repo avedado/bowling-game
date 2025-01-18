2b1db927a441f39b10282fe49d070e5fc3bd1663
DeEmilyRose <emilyrangel0711@gmail.com>
2025-01-18 23:28:05 +0100

    función spares

--- src/scoreCard.py
+++ src/scoreCard.py
@@ -4,9 +4,9 @@ class ScoreCard:
     def __init__(self, score_card):
         self.pins = score_card #lanzamientos
 
-        self.score = 0 #puntaje
+        '''self.score = 0 #puntaje
         self.frame = 10 #turnos
-        self.intentos = 0 #cambiar nombre
+        self.intentos = 0 #cambiar nombre'''
 
     def get_pins(self):
         return self.pins
@@ -20,3 +20,20 @@ class ScoreCard:
         for pin in self.pins:
             pin == 'X'
             return 300
+        #por cada pin en pins, si es igual a 'X' devuelve 300
+        #es una función muy específica
+
+    def spares(self):
+        frame_spares = list()
+        for pin in self.pins:
+            if pin == '5':
+                frame_spares.append(pin)
+            elif pin == '/':
+                frame_spares.append(pin)
+                return 150
+        #por cada pin en pins, si es igual a '5'
+        #agregalo a la lista, si el igual a '/', 
+        #tambien y devuelve 150.
+        #es una función muy específica
+
+    
